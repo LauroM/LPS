4a9d7835e3f3254c308d79c4aaa997caff2e5b4c
Lucas Ponce <lucasdpleon@gmail.com>
2020-06-04 17:59:37 -0300

    primeira tela

---
+++ now-ui-kit-angular-master/src/app/components/quem-somos/quem-somos.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { QuemSomosComponent } from './quem-somos.component';
+
+
+
+@NgModule({
+  declarations: [QuemSomosComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class QuemSomosModule { }
